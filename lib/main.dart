a560718d80e4d78a5d289180457a49faefc06852
mukae9 <rockjoint2016@gmail.com>
2024-05-22 14:07:40 +0900

    Androidリリース

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mola_gemini_flutter_template/presentation/app_page.dart';
 import 'package:provider/provider.dart';
 
 import 'app_config.dart';
+import 'common/access_url.dart';
 import 'di_container.dart';
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) {
+        final isValidHost = [
+          productionUrl,
+        ].contains(host);
+        return isValidHost;
+      };
+  }
+}
 
 Future<void> main() async {
   ///各種アプリの設定を読み取り
+  HttpOverrides.global = MyHttpOverrides();
   await dotenv.load(fileName: ".env");
   await configure();
   runApp(
