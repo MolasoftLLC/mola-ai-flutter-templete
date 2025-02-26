a900d51d14f2bf95aa521539891fd9244553c357
mukae9 <rockjoint2016@gmail.com>
2025-02-26 17:12:56 +0900

    Revert "メニュー検索機能の改善: 新しいAPIエンドポイントの統合とUX向上"

--- lib/infrastructure/api_client/sake_menu_recognition_api_client.dart
+++ lib/infrastructure/api_client/sake_menu_recognition_api_client.dart
@@ -12,15 +12,4 @@ abstract class SakeMenuRecognitionApiClient extends ChopperService {
   Future<Response> recognizeMenu(
     @Part() String file,
   );
-  
-  @Post(path: 'extract')
-  @Multipart()
-  Future<Response> extractSakeInfo(
-    @Part() String file,
-  );
-  
-  @Post(path: 'extract')
-  Future<Response> extractSakeInfoJson(
-    @Body() Map<String, dynamic> body,
-  );
 }
